dc25d64a6475347ffc3cb9fc3265b920ecad1d69
AguynamedRich <imliuruiqi@gmail.com>
2018-10-15 17:05:39 +0800

    自定义书籍 科技新闻

---
+++ books/Technews.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+from base import BaseFeedBook
+def getBook():
+    return Technews
+class Technews(BaseFeedBook):
+    title                 = u'科技新闻'
+    description           = u'科技新闻精选,少数派，好奇心日报,36kr,果壳...'
+    language              = 'zh-cn'
+    feed_encoding         = "utf-8"
+    page_encoding         = "utf-8"
+    mastheadfile          = "mh_ftchinese.gif"
+    coverfile             = "cv_ftchinese.jpg"
+    oldest_article        = 1
+    feeds = [
+            (u'少数派', 'http://sspai.com/feed'),
+            (u'好奇心日报', 'http://www.qdaily.com/feed'),
+            ('36kr', 'http://www.36kr.com/feed?1.0'),
+            (u'小道消息', 'http://hutu.me/feed'),
+            (u'极客公园', 'http://www.geekpark.net/rss'),
+            (u'TechCrunch 中国', 'http://techcrunch.cn/feed/'),
+            (u'爱范儿', 'http://www.ifanr.com/feed'),
+            (u'科学松鼠会', 'http://songshuhui.net/feed'),
+            (u'果壳','http://feeds.brandipo.com/users/1/web_requests/21/guoke.xml'),
+            ('cnBeta','http://feedmaker.kindle4rss.com/feeds/cnbeta.xml'),
+            (u'月光博客','http://feed.williamlong.info/'),
+            (u'DeepTech深科技','http://feedmaker.kindle4rss.com/feeds/mit-tr.weixin.xml'),
+            (u'InfoQ中文','http://feedmaker.kindle4rss.com/feeds/cn.infoq.xml',True),
+            (u'Engadget中文版','http://feedmaker.kindle4rss.com/feeds/cn.engadget.com.xml',True),
+            (u'环球科学', 'http://blog.sina.com.cn/rss/sciam.xml'),
+            ]
+def fetcharticle(self, url, opener, decoder):
+        #每个URL都增加一个后缀full=y，如果有分页则自动获取全部分页
+        url += '?full=y'
+        return BaseFeedBook.fetcharticle(self,url,opener,decoder)
